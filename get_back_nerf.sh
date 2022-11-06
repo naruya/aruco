72481179f27e7ebaacccf4ebd3f575eb11eacd98
naruya <naruya.kondo@gmail.com>
2022-11-06 17:26:57 +0000

    update

--- get_back_nerf.sh
+++
@@ -1,9 +0,0 @@
-if [ $# != 2 ]; then
-  echo invalid args!
-  exit 2
-fi
-
-python get_nerf_dataset.py --vid /root/workspace/data/record3d/tonpy-$1/raw/back.mp4
-mv charuco /root/workspace/data/record3d/tonpy-$1
-./nerfstudio_train.sh $1
-
